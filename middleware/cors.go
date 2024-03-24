c530faef24c679b5f323c2dd21dfc0733d8ffa8a
Javod Mutalliboev <javodmutalliboev@gmail.com>
2024-03-24 18:11:40 +0500

    cors updated

--- middleware/cors.go
+++ middleware/cors.go
@@ -8,10 +8,18 @@ import (
 // CORSMiddleware sets the appropriate CORS headers
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", os.Getenv("ADMIN_ORIGIN"))
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PATCH, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-		w.Header().Set("Access-Control-Allow-Credentials", "true") // Add this line
+		allowedOrigins := map[string]bool{
+			os.Getenv("ADMIN_ORIGIN"):  true,
+			os.Getenv("CLIENT_ORIGIN"): true,
+			// Add more origins if you want
+		}
+
+		if _, ok := allowedOrigins[r.Header.Get("Origin")]; ok {
+			w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PATCH, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+		}
 
 		// If it's a preflight request, respond with 200
 		if r.Method == "OPTIONS" {
