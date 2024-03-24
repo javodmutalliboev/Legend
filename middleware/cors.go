5857f68325d583104a83d101075905561aa0884c
Javod Mutalliboev <javodmutalliboev@gmail.com>
2024-03-24 16:52:10 +0500

    cors middleware updated

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,13 +1,17 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 // CORSMiddleware sets the appropriate CORS headers
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", os.Getenv("ADMIN_ORIGIN"))
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PATCH, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true") // Add this line
 
 		// If it's a preflight request, respond with 200
 		if r.Method == "OPTIONS" {
