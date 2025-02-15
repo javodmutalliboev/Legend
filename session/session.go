af4254c64fd7082cacbe4c8fee5d59c44f18bbb0
Javod Mutalliboev <javodmutalliboev@gmail.com>
2025-02-15 16:56:19 +0500

    updated for cookie delivery

--- session/session.go
+++ session/session.go
@@ -38,7 +38,7 @@ func SaveOptions(session *sessions.Session, MaxAge int /* in seconds */) {
 		MaxAge:/* 1 day */ MaxAge,
 		HttpOnly: true,
 		Secure:   true,
-		Domain:   "online-dukon-admin.netlify.app",
+		Domain:   os.Getenv("ADMIN_ORIGIN_COOKIE"),
 		SameSite: http.SameSiteNoneMode,
 	}
 }
