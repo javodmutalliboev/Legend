2fe417bacf83268af0017a7a74e00bf448f3011b
Javod Mutalliboev <javodmutalliboev@gmail.com>
2025-02-15 18:47:32 +0500

    updated for cookie delivery

--- session/session.go
+++ session/session.go
@@ -38,7 +38,6 @@ func SaveOptions(session *sessions.Session, MaxAge int /* in seconds */) {
 		MaxAge:/* 1 day */ MaxAge,
 		HttpOnly: true,
 		Secure:   true,
-		Domain:   os.Getenv("ADMIN_ORIGIN_COOKIE"),
 		SameSite: http.SameSiteNoneMode,
 	}
 }
